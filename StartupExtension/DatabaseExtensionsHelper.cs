aecd51b55b0b7991f08fe4da377677e6ee28a5d6
FaizulDevSkill2025 <csldotnet@gmail.com>
2026-07-03 12:11:09 +0600

    Updated

---
+++ StartupExtension/DatabaseExtensionsHelper.cs
@@ -0,0 +1,35 @@
+﻿using Demo.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.StartupExtension
+{
+    public static class DatabaseExtensionsHelper
+    {
+
+        public static IServiceCollection AddDatabaseExtensionHelper(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), sqlServerOptionsAction => sqlServerOptionsAction.EnableRetryOnFailure());
+
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder RunMigration(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                db.Database.Migrate();
+
+            }
+
+            return app;
+
+        }
+    }
+}
